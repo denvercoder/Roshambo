9e0a5fb1b304fe0449e1f4bf11b479dfdbdab15d
Tim Myers <timmyers1978@gmail.com>
2016-10-24 15:51:37 -0600

    add ResultViewController, add graphics

--- RoShamBo/ResultViewController.swift
+++ RoShamBo/ResultViewController.swift
@@ -1,5 +1,5 @@
 //
-//  PlayViewController.swift
+//  ResultViewController.swift
 //  RoShamBo
 //
 //  Created by Timothy Myers on 10/24/16.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class PlayViewController: UIViewController {
-
+class ResultViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
