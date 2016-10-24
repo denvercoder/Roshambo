93c54921c7d97c79f84923aaa78baaf30be56db5
Tim Myers <timmyers1978@gmail.com>
2016-10-24 17:27:12 -0600

    done but with errors

--- RoShamBo/PlayViewController.swift
+++ RoShamBo/PlayViewController.swift
@@ -20,6 +20,53 @@ class PlayViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rockPlayed(sender: AnyObject) {
+        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+        
+        controller.playerPlayed = "rock"
+        
+        let computer = randomPlay()
+        
+        controller.computerPlayed = computer
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
 
+    @IBAction func paperPlayed(sender: AnyObject) {
+        self.performSegueWithIdentifier("paperPlayed", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! ResultViewController
+        let computer = randomPlay()
+        controller.computerPlayed = computer
+        
+        if segue.identifier == "paperPlayed" {
+            controller.playerPlayed = "paper"
+        } else if segue.identifier == "scissorsPlayed" {
+            controller.playerPlayed = "scissors"
+        }
+    }
+    
+    func randomPlay() -> String {
+        var choice: Int
+        
+        choice = Int(1 + arc4random() % 3)
+        
+        switch choice {
+        case 1:
+            return "rock"
+        case 2:
+            return "paper"
+        case 3:
+            return "scissors"
+        default:
+            print("An error Occured")
+            return "An Error Occured!"
+        }
+    }
+    
+    
 }
 
--- RoShamBo/ResultViewController.swift
+++ RoShamBo/ResultViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var playerPlayed: String!
+    var computerPlayed: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        self.whoWon(playerPlayed, computerPlayed: computerPlayed)
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +28,51 @@ class ResultViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func whoWon(playerPlayed: String, computerPlayed: String) -> Int {
+        
+        if playerPlayed == computerPlayed {
+            displayResults("itsATie.png", message: "It's a tie your dad would love!")
+            return 2
+        }
+        
+        if playerPlayed == "rock" {
+            if computerPlayed == "scissors" {
+                displayResults("RockCrushesScissors.jpeg", message: "Rock crushes scissors, You Win!")
+                return 1
+            }
+        }
+        
+        if playerPlayed == "paper" {
+            if computerPlayed == "rock" {
+                displayResults("PaperCoversRock.jpg", message: "Paper covers Rock, You Win!")
+                return 1
+            }
+            if computerPlayed == "scissors" {
+                displayResults("ScissorsCutPaper.jpg", message: "Scissors cuts paper, You Lose :(")
+                return 0
+            }
+        }
+        
+        if playerPlayed == "scissors" {
+            if computerPlayed == "paper" {
+                displayResults("ScissorsCutPaper.jpg", message: "Scissors cuts paper, You Win!")
+                return 1
+            }
+            if computerPlayed == "rock" {
+                displayResults("RockCrushesScissors.jpeg", message: "Rock smashes scissors, You Lose :(")
+                return 0
+            }
+        }
+      
+        return -1
+    }
     
+    func displayResults(result: String, message: String) {
+        self.image.image = UIImage(named: result)
+        self.label.text = message
+    }
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
